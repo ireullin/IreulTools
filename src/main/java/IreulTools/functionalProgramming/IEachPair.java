bcd7075e65a339af355e352eee70fa5c0fd0c237
ireullin <ireullin@gmail.com>
2018-02-27 16:31:13 +0800

    Added a declaration for throwing exceptions

--- src/main/java/IreulTools/functionalProgramming/IEachPair.java
+++ src/main/java/IreulTools/functionalProgramming/IEachPair.java
@@ -4,5 +4,5 @@ package IreulTools.functionalProgramming;
  * Created by ireullin on 2016/10/22.
  */
 public interface IEachPair<U,K,V> {
-    U put(K k,V v);
+    U put(K k,V v) throws Exception;
 }
