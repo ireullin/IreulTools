95ce63ab616615456d531e30c0c64d8af6fafe60
ireullin <ireullin@gmail.com>
2018-03-05 16:49:54 +0800

    Added a new class MutableDatetime

--- src/main/java/IreulTools/datetime/MutableDatetime.java
+++ src/main/java/IreulTools/datetime/MutableDatetime.java
@@ -1,4 +1,308 @@
 package IreulTools.datetime;
 
-public class MutableDatetime {
+import IreulTools.functionalProgramming.ITap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class MutableDatetime implements IDatetime {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MutableDatetime.class);
+
+    private IDatetime immutableDatetime;
+
+    public static IDatetime readFrom(String dtstr, String format) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        Date date = sdf.parse(dtstr);
+        TimeZone tz = sdf.getTimeZone();
+        MutableDatetime dt = new MutableDatetime(date, tz);
+        return dt;
+    }
+
+    public static IDatetime now(){
+        return new MutableDatetime();
+    }
+
+    public static IDatetime  zeroDay(){
+        MutableDatetime dt = new MutableDatetime(0);
+        return dt.toUTC();
+    }
+
+    private MutableDatetime(){
+        this.immutableDatetime = ImmutableDatetime.now();
+    }
+
+    public static IDatetime of(TimeZone tz){
+        return new MutableDatetime(tz);
+    }
+
+    private MutableDatetime(TimeZone tz){
+        this.immutableDatetime = ImmutableDatetime.of(tz);
+    }
+
+    public static IDatetime of(Date dt){
+        return new MutableDatetime(dt);
+    }
+
+    private MutableDatetime(Date dt){
+        this.immutableDatetime = ImmutableDatetime.of(dt);
+    }
+
+    public static IDatetime of(Date dt, TimeZone tz){
+        return new MutableDatetime(dt, tz);
+    }
+
+    private MutableDatetime(Date dt, TimeZone tz){
+        this.immutableDatetime = ImmutableDatetime.of(dt, tz);
+    }
+
+    public static IDatetime of(IReadOnlyDatetime datetime){
+        return new MutableDatetime(datetime);
+    }
+
+    private MutableDatetime(IReadOnlyDatetime datetime){
+        this.immutableDatetime = ImmutableDatetime.of(datetime);
+    }
+
+    public static IDatetime of(long stamp){
+        return new MutableDatetime(stamp);
+    }
+
+    private MutableDatetime(long stamp){
+        this.immutableDatetime = ImmutableDatetime.of(stamp);
+    }
+
+    public static IDatetime of(long stamp, TimeZone tz){
+        return new MutableDatetime(stamp, tz);
+    }
+
+    private MutableDatetime(long stamp, TimeZone tz){
+        this.immutableDatetime = ImmutableDatetime.of(stamp, tz);
+    }
+
+    public static IDatetime of(int year, int month, int day){
+        return new MutableDatetime(year,month,day,0,0,0,0);
+    }
+
+    public static IDatetime of(int year, int month, int day, TimeZone tz){
+        return new MutableDatetime(year,month,day,0,0,0,0,tz);
+    }
+
+    public static IDatetime of(int year, int month, int day, int hour, int min, int sec, int millis) {
+        return new MutableDatetime(year, month, day, hour, min, sec, millis);
+    }
+
+    public static IDatetime of(int year, int month, int day, int hour, int min, int sec, int millis, TimeZone tz){
+        return new MutableDatetime(year, month, day, hour, min, sec, millis, tz);
+    }
+
+    private MutableDatetime(int year, int month, int day, int hour, int min, int sec, int millis){
+        this.immutableDatetime = ImmutableDatetime.of(year, month, day, hour, min, sec, millis);
+    }
+
+    private MutableDatetime(int year, int month, int day, int hour, int min, int sec, int millis, TimeZone tz){
+        this.immutableDatetime = ImmutableDatetime.of(year, month, day, hour, min, sec, millis, tz);
+    }
+
+    @Override
+    public IDatetime year(int v) {
+        this.immutableDatetime = this.immutableDatetime.year(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime month(int v) {
+        this.immutableDatetime = this.immutableDatetime.month(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime day(int v) {
+        this.immutableDatetime = this.immutableDatetime.day(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime hour(int v) {
+        this.immutableDatetime = this.immutableDatetime.hour(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime min(int v) {
+        this.immutableDatetime = this.immutableDatetime.min(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime sec(int v) {
+        this.immutableDatetime = this.immutableDatetime.sec(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime millis(int v) {
+        this.immutableDatetime = this.immutableDatetime.millis(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime addOrSubDay(int v) {
+        this.immutableDatetime = this.immutableDatetime.addOrSubDay(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime addOrSubHour(int v) {
+        this.immutableDatetime = this.immutableDatetime.addOrSubHour(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime addOrSubMin(int v) {
+        this.immutableDatetime = this.immutableDatetime.addOrSubMin(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime addOrSubSec(int v) {
+        this.immutableDatetime = this.immutableDatetime.addOrSubSec(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime addOrSubMillis(long v) {
+        this.immutableDatetime = this.immutableDatetime.addOrSubMillis(v);
+        return this;
+    }
+
+    @Override
+    public IDatetime reset(IReadOnlyDatetime dt) throws Exception {
+        this.immutableDatetime = ImmutableDatetime.of(dt.stamp(), dt.getTimeZone());
+        return this;
+    }
+
+    @Override
+    public IDatetime clone() {
+        return MutableDatetime.of(this.immutableDatetime);
+    }
+
+    @Override
+    public IDatetime toBeginOfDay() {
+        this.immutableDatetime = this.immutableDatetime.toBeginOfDay();
+        return this;
+    }
+
+    @Override
+    public IDatetime toEndOfDay() {
+        this.immutableDatetime = this.immutableDatetime.toEndOfDay();
+        return this;
+    }
+
+    @Override
+    public IDatetime toTimeZone(TimeZone tz) {
+        this.immutableDatetime = this.immutableDatetime.toTimeZone(tz);
+        return this;
+    }
+
+    @Override
+    public IDatetime toUTC() {
+        this.immutableDatetime = this.immutableDatetime.toUTC();
+        return this;
+    }
+
+    @Override
+    public IDatetime toLocalTime() {
+        this.immutableDatetime = this.immutableDatetime.toLocalTime();
+        return this;
+    }
+
+    @Override
+    public IDatetime tap(ITap<IReadOnlyDatetime> debugMsg) throws Exception {
+        debugMsg.put(this);
+        return this;
+    }
+
+    @Override
+    public IDuration during(IReadOnlyDatetime dt) {
+        return to(dt);
+    }
+
+    @Override
+    public IDuration to(IReadOnlyDatetime dt) {
+        return this.immutableDatetime.to(dt);
+    }
+
+    @Override
+    public IDuration from(IReadOnlyDatetime dt) {
+        return this.immutableDatetime.from(dt);
+    }
+
+    @Override
+    public int year() {
+        return this.immutableDatetime.year();
+    }
+
+    @Override
+    public int month() {
+        return this.immutableDatetime.month();
+    }
+
+    @Override
+    public int day() {
+        return this.immutableDatetime.day();
+    }
+
+    @Override
+    public int hour() {
+        return this.immutableDatetime.hour();
+    }
+
+    @Override
+    public int min() {
+        return this.immutableDatetime.min();
+    }
+
+    @Override
+    public int sec() {
+        return this.immutableDatetime.sec();
+    }
+
+    @Override
+    public int millis() {
+        return this.immutableDatetime.millis();
+    }
+
+    @Override
+    public long stamp() {
+        return this.immutableDatetime.stamp();
+    }
+
+    @Override
+    public TimeZone getTimeZone() {
+        return this.immutableDatetime.getTimeZone();
+    }
+
+    @Override
+    public String toString(String format) throws Exception {
+        return this.immutableDatetime.toString(format);
+    }
+
+    @Override
+    public String toString(){
+        return this.immutableDatetime.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MutableDatetime datetime = (MutableDatetime) o;
+        return datetime.stamp()==this.stamp();
+    }
 }
---
+++ src/test/java/IreulTools/datetime/MutableDatetimeTest.java
@@ -0,0 +1,212 @@
+package IreulTools.datetime;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertEquals;
+
+public class MutableDatetimeTest {
+    private static final Logger LOG = LoggerFactory.getLogger(MutableDatetimeTest.class);
+
+
+    @Test
+    public void testAddorSub() {
+
+        try {
+            IDatetime pre = MutableDatetime.of(2016, 5, 4, 23, 30, 59, 0);
+            IDatetime cur = pre.addOrSubDay(90);
+            LOG.info(cur.toString());
+            assertEquals("2016-08-02 23:30:59", cur.toString("yyyy-MM-dd HH:mm:ss"));
+        }
+        catch (Exception e){
+            LOG.error("error", e);
+            assertEquals(false,true);
+        }
+
+        try {
+            IDatetime pre = MutableDatetime.of(2016, 5, 4, 23, 30, 59, 0);
+            IDatetime cur = pre.addOrSubMillis(48*60*60*1000);
+            LOG.info(cur.toString());
+            assertEquals("2016-05-06 23:30:59", cur.toString("yyyy-MM-dd HH:mm:ss"));
+        }
+        catch (Exception e){
+            LOG.error("error", e);
+            assertEquals(false,true);
+        }
+
+
+        // 閏年
+        try {
+            IDatetime pre = MutableDatetime.of(2016, 2, 27, 23, 30, 59, 0);
+            IDatetime cur = pre.addOrSubDay(2);
+            LOG.info(cur.toString());
+            assertEquals("2016-02-29 23:30:59", cur.toString("yyyy-MM-dd HH:mm:ss"));
+        }
+        catch (Exception e){
+            LOG.error("error", e);
+            assertEquals(false,true);
+        }
+
+        // 沒閏年
+        try {
+            IDatetime pre = MutableDatetime.of(2015, 2, 27, 23, 30, 59, 0);
+            IDatetime cur = pre.addOrSubDay(2);
+            LOG.info(cur.toString());
+            assertEquals("2015-03-01 23:30:59", cur.toString("yyyy-MM-dd HH:mm:ss"));
+        }
+        catch (Exception e){
+            LOG.error("error", e);
+            assertEquals(false,true);
+        }
+
+    }
+
+    @Test
+    public void testZeroDay() {
+        try {
+            IDatetime dt1 = MutableDatetime.zeroDay();
+            LOG.info(dt1.getTimeZone().toString());
+            LOG.info(dt1.toString());
+
+            IDatetime dt2 = MutableDatetime.readFrom("1970-01-01 08:00:00.000", "yyyy-MM-dd HH:mm:ss.SSS");
+            dt2.toUTC();
+            LOG.info(dt2.getTimeZone().toString());
+            LOG.info(dt2.toString());
+            assertEquals(dt1.equals(dt2) , true);
+        }
+        catch (Exception e){
+            LOG.error("error", e);
+            assertEquals(false,true);
+        }
+    }
+
+
+    @Test
+    public void testReset() throws Exception{
+        String sample = "2016-08-04 13:34:56";
+        IDatetime dt1 = MutableDatetime.readFrom(sample, "yyyy-MM-dd HH:mm:ss");
+        LOG.info(dt1.toString("yyyy-MM-dd HH:mm:ss a"));
+
+        IDatetime dt2 = MutableDatetime.now();
+        LOG.info(dt2.toString("yyyy-MM-dd HH:mm:ss"));
+        dt2.reset(dt1);
+        LOG.info(dt2.toString("yyyy-MM-dd HH:mm:ss a"));
+        assertEquals(dt2.toString("yyyy-MM-dd HH:mm:ss"), sample);
+
+    }
+
+    @Test
+    public void testTimeZone() {
+        try {
+            IDatetime dt1 = MutableDatetime.now();
+            LOG.info("{} {}", dt1.toString("yyyy-MM-dd HH:mm:ss zzz "), dt1.getTimeZone());
+
+            IDatetime dtutc = dt1.toUTC();
+            LOG.info("{} {}", dtutc.toString("yyyy-MM-dd HH:mm:ss zzz "), dtutc.getTimeZone());
+            LOG.info("{} {}", dt1.toString("yyyy-MM-dd HH:mm:ss zzz "), dt1.getTimeZone());
+
+            IDatetime dt2 = dt1.toLocalTime();
+            LOG.info("{} {}", dt2.toString("yyyy-MM-dd HH:mm:ss zzz "), dt2.getTimeZone());
+
+
+            IDatetime dt3 = MutableDatetime.readFrom("2016-10-25 16:07:21 TST", "yyyy-MM-dd HH:mm:ss zzz");
+            LOG.info("{} {}", dt3.toString("yyyy-MM-dd HH:mm:ss zzz"), dt3.getTimeZone());
+
+            //!!!!!!!!!! it will get CEST. I don't know why ????????
+            IDatetime dt4 = MutableDatetime.readFrom("2016-10-25 16:07:21 UTC", "yyyy-MM-dd HH:mm:ss zzz");
+            LOG.info("{} {}", dt4.toString("yyyy-MM-dd HH:mm:ss zzz"), dt4.getTimeZone());
+
+            IDatetime dt5 = MutableDatetime.readFrom("2016-10-25 16:07:21", "yyyy-MM-dd HH:mm:ss");
+            LOG.info("{} {}", dt5.toString("yyyy-MM-dd HH:mm:ss zzz"), dt5.getTimeZone());
+
+        }
+        catch (Exception e){
+            LOG.error("error", e);
+        }
+
+    }
+
+    @Test
+    public void testMain() {
+
+        IDatetime dt1 = MutableDatetime.now().year(2012).month(2).day(15).hour(6).min(10).sec(5).millis(200);
+        IDatetime dt2 = MutableDatetime.of(2012,2,15,6,10,5,200);
+        LOG.info(dt1.toString());
+        LOG.info(dt2.toString());
+        assertEquals(dt1.equals(dt2),true);
+
+        IDatetime dt3 = MutableDatetime.of(2015,8,4,9,10,15,000);
+
+        LOG.info(dt3.toString());
+        assertEquals("2015-08-04 09:10:15.000", dt3.toString());
+
+        dt3.toBeginOfDay();
+        LOG.info(dt3.toString());
+        assertEquals("2015-08-04 00:00:00.000", dt3.toString());
+
+        dt3.toEndOfDay();
+        LOG.info(dt3.toString());
+        assertEquals("2015-08-04 23:59:59.999", dt3.toString());
+
+        LOG.info("{} stamp:{}", dt3.toString(), dt3.stamp());
+        assertEquals(1438703999999l, dt3.stamp());
+
+        LOG.info("==============================");
+
+        IDatetime dt4 = MutableDatetime.of(dt3.stamp());
+        LOG.info(dt4.toString());
+        assertEquals(dt3.stamp(), dt4.stamp());
+        LOG.info("==============================");
+
+        try {
+            IDatetime dt5 = MutableDatetime.readFrom("20140825130972321", "yyyyMMddHHmmssSSS");
+            LOG.info(dt5.toString());
+            assertEquals("2014-08-25 13:10:12.321",dt5.toString());
+        }
+        catch (Exception e){
+            LOG.error("format error",e);
+            assertEquals(false,true);
+        }
+        LOG.info("==============================");
+
+        try{
+            IDatetime dt6 = MutableDatetime.readFrom("2010-03-01 10:00:00","yyyy-MM-dd HH:mm:ss") .tap(dt -> LOG.info("Started at"+dt.toString()) )
+                    .addOrSubDay(-3).tap( dt -> LOG.info("sub {} days is {}", 3 ,  dt.toString()) )
+                    .addOrSubHour(73).tap( dt -> LOG.info("add {} hours is {}", 73 ,  dt.toString()) )
+                    .addOrSubMin(-120).tap( dt -> LOG.info("sub {} minutes is {}", 120 ,  dt.toString()) )
+                    .addOrSubSec(5).tap( dt -> LOG.info("add {} second is {}", 5 ,  dt.toString()) )
+                    .addOrSubMillis(1500).tap( dt -> LOG.info("add {} millisecond is {}", 1500 ,  dt.toString()) );
+
+            LOG.info(dt6.toString());
+            assertEquals("2010-03-01 09:00:06.500",dt6.toString());
+        }
+        catch (Exception e){
+            LOG.error("format error",e);
+        }
+        LOG.info("==============================");
+
+        IDatetime dtA = MutableDatetime.now();
+        IDatetime dtB = dtA.clone().addOrSubDay(-1)
+                .addOrSubHour(2)
+                .addOrSubMin(3)
+                .addOrSubSec(5)
+                .addOrSubMillis(600);
+
+//        IDuration du = dtA.during(dtB);
+        IDuration duTo = dtA.to(dtB);
+        IDuration duFrom = dtB.from(dtA);
+        LOG.info("A is {} ({}), B is {} ({})", dtA.toString(), dtA.stamp(), dtB.toString(), dtB.stamp());
+        LOG.info("duration is {} ({})", duTo.toString(), duTo.stamp());
+        LOG.info("total {} days", duTo.totalDay());
+        LOG.info("total {} hours", duTo.totalHour());
+        LOG.info("total {} minutes", duTo.totalMin());
+        LOG.info("total {} seconds", duTo.totalSec());
+        LOG.info("total {} milliseconds", duTo.totalMillis());
+
+        IDatetime dtC = dtA.addOrSubMillis(duTo.totalMillis());
+        assertEquals( duTo.totalMillis(), duFrom.totalMillis() );
+        assertEquals( dtC.stamp(), dtB.stamp() );
+    }
+
+}
